1333c779975dd8ed707758bcf3165ea6b651dac2
haimi <webmaster@haimi.de>
2026-01-31 03:16:38 +0100

    floating mask

---
+++ Assets/scripts/float.gd
@@ -0,0 +1,19 @@
+extends StaticBody3D
+
+@export var float_speed: float = 1.0
+@export var float_limit: float = 1.0
+@export var rotate_speed: float = 2.0
+
+var floatStart: float;
+var floatProgress: float;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	floatStart = position.y
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	floatProgress += delta
+	position.y = floatStart + (sin(floatProgress) + .5) * float_limit;
+	rotate_y(rotate_speed * delta)

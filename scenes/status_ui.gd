818386a4058618f26d0527d893378d307ebc8f9c
haimi <webmaster@haimi.de>
2026-01-31 14:58:18 +0100

    UI mask change

--- scenes/status_ui.gd
+++ scenes/status_ui.gd
@@ -5,27 +5,37 @@ const HP_HIGH = 5
 
 var max_hitpoints: int = HP_DEFAULT
 var cur_hitpoints: int;
-var cur_mask: String = ""
+var cur_mask: String = "Default"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	cur_hitpoints = max_hitpoints
 
+func hit():
+	print("hit")
+	cur_hitpoints -= 1
+	if cur_hitpoints == 1:
+		var sm: ShaderMaterial = $SmileyPanel.find_child(cur_mask).material
+		sm.set_shader_parameter("active", true)
+	updateHitpoints()
+
 func changeMask(mask: String) -> void:
 	cur_mask = mask
-	if cur_mask in ['strong']:
+	if cur_mask in ['Smiley']:
 		max_hitpoints = HP_HIGH
 		cur_hitpoints = HP_HIGH
 	else:
 		max_hitpoints = HP_DEFAULT
 		cur_hitpoints = HP_DEFAULT
+	var sm: ShaderMaterial = $SmileyPanel.find_child(cur_mask).material
+	sm.set_shader_parameter("active", false)
 	updateMask()
 	updateHitpoints()
 
 func updateMask():
 	for mask in $SmileyPanel.get_children():
 		mask.visible = false
-	#$SmileyPanel.find_child(cur_mask, false).visible = true
+	$SmileyPanel.find_child(cur_mask, false).visible = true
 
 func updateHitpoints():
 	$TextPanel/Label.text = "Life {0}/{1}".format([cur_hitpoints, max_hitpoints])

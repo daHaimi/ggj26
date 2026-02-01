4daa69b1e8cefa4f1624ff63faa5241a2a120a9d
rellig <rellig@minad.de>
2026-02-01 14:55:38 +0100

    implement background loading of level in intro scene

--- Assets/scripts/intro.gd
+++ Assets/scripts/intro.gd
@@ -1,7 +1,8 @@
 extends CanvasLayer
 
-var level: Resource
+func _ready():
+	ResourceLoader.load_threaded_request("res://scenes/level.tscn")
 
 func _input(event):
 	if event.is_action("ui_cancel") or event.is_action("attack"):
-		get_tree().change_scene_to_file("res://scenes/level.tscn")
+		get_tree().change_scene_to_node(ResourceLoader.load_threaded_get("res://scenes/level.tscn").instantiate())

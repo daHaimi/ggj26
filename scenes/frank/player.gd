5de94455de0ae97e3bb0fca5aad101c3ca839646
rellig <rellig@minad.de>
2026-02-01 13:28:48 +0100

    update controller support

--- scenes/frank/player.gd
+++ scenes/frank/player.gd
@@ -130,7 +130,7 @@ func _physics_process(delta: float) -> void:
 		#velocity.y = JUMP_VELOCITY
 
 
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("dash"):
 		dash()
 
 	var speed_used = SPEED

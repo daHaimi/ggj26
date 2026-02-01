1a75c708a4f2cb18f1bb2c54f7d858cf41148319
rellig <rellig@minad.de>
2026-02-01 11:22:21 +0100

    implement menu:

--- Assets/scripts/credits.gd
+++ Assets/scripts/credits.gd
@@ -6,11 +6,15 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	animator_hitman.play("hiphop-dancing")
 	animator_hitman.get_animation("hiphop-dancing").loop_mode = Animation.LOOP_LINEAR
+	animator_hitman.play("hiphop-dancing")
 	animator_player.play("hiphop-dancing")
 	animator_redneck.play("hiphop-dancing")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _input(event):
+	if event.is_action("ui_cancel") or event.is_action("attack"):
+		get_tree().change_scene_to_file("res://scenes/menu.tscn")
